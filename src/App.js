8a16b04a0296e9b2c23b6e93e49dfd5978f7a982
Neven.Gjoreski <n.gjoreski@blexr.com>
2020-02-24 15:23:30 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,88 +2,82 @@ import React, {useState} from 'react';
 import './App.css';
 import TreeComponent from "./components/TreeComponent";
 
-const data = {
-    name: '/root',
-    children: [
-        {
-            name: 'music',
-            children: [
-                {
-                    name: 'Bellyache.mp3'
-                },
-                {
-                    name: 'Mamma Mia.mp3'
-                },
-                {
-                    name: 'After Dark.mp3'
-                },
-                {
-                    name: 'Two Steps From Hell',
-                    children : [
-                        {
-                            name : 'hero.mp3'
-                        },
-                        {
-                            name : 'victory.mp3'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'movies',
-            children: [
-                {
-                    name: 'Lord Of The Rings.avi'
-                },
-                {
-                    name: 'Mamma Mia.avi'
-                },
-            ]
-        },
-        {
-            name: 'series',
-            children: [
-                {
-                    name: 'Gossip Girl.avi'
-                },
-                {
-                    name: 'Narcos.avi'
-                },
-                {
-                    name: 'Game of Thrones.avi'
-                },
-                {
-                    name: 'Favourites',
-                    children : [
-                        {
-                            name : 'Stranger Things.avi'
-                        },
-                        {
-                            name : 'La Casa De Papel.avi'
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-}
-
 let statuses = new Map();
 
 function App() {
 
     let [forceExpanded, setforceExpanded] =  useState({forced : false, value : false});
     let [expandedAll, setexpandedAll] =  useState(false);
-
-    console.log(1111111111111111)
+    let [data] = useState({
+        name: '/root',
+        subchildren: [
+            {
+                name: 'music',
+                subchildren: [
+                    {
+                        name: 'Bellyache.mp3'
+                    },
+                    {
+                        name: 'Mamma Mia.mp3'
+                    },
+                    {
+                        name: 'After Dark.mp3'
+                    },
+                    {
+                        name: 'Two Steps From Hell',
+                        subchildren : [
+                            {
+                                name : 'hero.mp3'
+                            },
+                            {
+                                name : 'victory.mp3'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'movies',
+                subchildren: [
+                    {
+                        name: 'Lord Of The Rings.avi'
+                    },
+                    {
+                        name: 'Mamma Mia.avi'
+                    },
+                ]
+            },
+            {
+                name: 'series',
+                subchildren: [
+                    {
+                        name: 'Gossip Girl.avi'
+                    },
+                    {
+                        name: 'Narcos.avi'
+                    },
+                    {
+                        name: 'Game of Thrones.avi'
+                    },
+                    {
+                        name: 'Favourites',
+                        subchildren : [
+                            {
+                                name : 'Stranger Things.avi'
+                            },
+                            {
+                                name : 'La Casa De Papel.avi'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    })
     const componentStatus = ( id, data ) => {
 
         statuses.set(id , data)
 
-        // console.log(statuses)
-        // TODO not status by name , by uuid
-        //will return 0 if all are expanded
         if( ([...statuses.values()].filter((element) => element === false ).length ) === 0) {
             setexpandedAll(true)
         } else {
--- src/components/TreeComponent.js
+++ src/components/TreeComponent.js
@@ -7,7 +7,7 @@ function TreeComponent(props) {
     let [expanded, setExpanded] = useState(false)
     // const [id] = useState(uuid())
 
-    const children = props.data.children
+    const subchildren = props.data.subchildren
     const name = props.data.name
     const depth = props.depth || 1
 
@@ -19,12 +19,12 @@ function TreeComponent(props) {
     }, [props.forceExpanded])
 
     useEffect(() => {
-        if ( children && children.length > 0){
+        if ( subchildren && subchildren.length > 0){
             props.componentStatus(name ,expanded)
         } else {
             props.componentStatus(name ,true)
         }
-    }, [expanded])
+    }, [subchildren, expanded, name, props])
 
     //render
     return (
@@ -34,15 +34,15 @@ function TreeComponent(props) {
                 onClick={() => setExpanded(!expanded)}
             >
                 <div>
-                    <SymbolSpan expanded={expanded} children={children} name={name}/>
+                    <SymbolSpan expanded={expanded} subchildren={subchildren} name={name}/>
                 </div>
             </div>
 
             {
-                children
-                && children.length > 0
+                subchildren
+                && subchildren.length > 0
                 && expanded
-                && children.map((v, i) => {
+                && subchildren.map((v, i) => {
                     return <TreeComponent depth={depth + 1} key={i} data={v}
                         expanded={expanded} forceExpanded={props.forceExpanded}
                         componentStatus={props.componentStatus}/>
@@ -57,7 +57,7 @@ function TreeComponent(props) {
 const SymbolSpan = (props) => {
     let symbol
 
-    if (props.children) {
+    if (props.subchildren) {
         symbol = props.expanded ? '\u25BC' : '\u2BC8'
     } else {
         symbol = '\u2802 '
